d9fbf81c4b492311c0dfa23583700bf4c1981a92
Joshua Fortriede <jdfortriede@gmail.com>
2022-05-06 07:58:15 -0400

    updated schema version

--- modules/schemaFunctions.py
+++ modules/schemaFunctions.py
@@ -122,7 +122,7 @@ def load_data_fields(validator):
         json_data = json.load(fh_json_file)
         return json_data['properties']
 class ImmPort_Data: 
-    schemaVersion = "3.34"
+    schemaVersion = "3.35"
 
     def print_obj(self):
         print_data = {}
