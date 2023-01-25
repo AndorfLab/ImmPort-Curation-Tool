113e9bc8c493af4091cc0940809451569136655e
JoshuaFortriede <51160626+JoshuaFortriede@users.noreply.github.com>
2023-01-25 16:37:58 -0500

    data_files fix: specify json-templates

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     keywords="ImmPort Curation",
     install_requires=get_required_packages(),
     packages=[package_name],
-    data_files=[('ImmPortCurationTool/templates',['ImmPortCurationTool/templates/*'])],
+    data_files=[('ImmPortCurationTool/templates/json-templates',['ImmPortCurationTool/templates/json-templates/*'])],
     python_requires=">=3.7",
     zip_safe=False,
     url=git_url,
